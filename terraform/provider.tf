f7dbc9599dee12df270522d9b5e4267d8ba57ec1
KimGilSu <hwanrim00@gmail.com>
2023-11-24 16:40:26 +0900

    aws provider with vault

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
-  
+  region     = "ap-northeast-2"
+  access_key = data.hcp_vault_secrets_app.aws_app.secrets.k
+  secret_key = data.hcp_vault_secrets_app.aws_app.secrets.s
 }
+
+
+data "hcp_vault_secrets_app" "aws_app" {
+  app_name = "AWS"
+}
+
